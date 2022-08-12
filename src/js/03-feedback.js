bb7b22c2fc034de0b1d479d73268f371ff408f58
ArtemTeslenko <artem.teslenko.w126@gmail.com>
2022-08-12 17:23:27 +0300

    Update 03-feedback.js
    Showed data from locale starage. Completed but there are questions about console after submit. Found bug in 2 part of HW, when video is finished. Must fix!

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,27 +1,44 @@
+import throttle from 'lodash.throttle';
+
 const FEEDBACK_FORM_KEY = 'feedback-form-state';
 const formEl = document.querySelector('.feedback-form');
-// const emailEl = document.querySelector('.feedback-form input');
-// const messageEl = document.querySelector('.feedback-form textarea');
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
 const formStateData = {};
 
-formEl.addEventListener('input', saveValue);
-formEl.addEventListener('submit', saveString);
+populateTextarea();
 
-function saveValue(e) {
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormInput(e) {
   if (e.target.nodeName === 'INPUT') {
     formStateData.email = e.target.value;
   }
   if (e.target.nodeName === 'TEXTAREA') {
     formStateData.feedback = e.target.value;
+    console.log(textareaEl.value);
   }
 
-  let formData = JSON.stringify(formStateData);
+  const formData = JSON.stringify(formStateData);
   localStorage.setItem(FEEDBACK_FORM_KEY, formData);
-  console.log(formData);
 }
 
-function saveString(e) {
+function onFormSubmit(e) {
   e.preventDefault();
+  e.target.reset();
+  localStorage.removeItem(FEEDBACK_FORM_KEY);
+  console.log(formStateData);
 }
 
-console.log(formStateData);
+function populateTextarea() {
+  const savedFormData = localStorage.getItem(FEEDBACK_FORM_KEY);
+  const formDataObj = JSON.parse(savedFormData);
+
+  if (savedFormData) {
+    formStateData.email = formDataObj.email;
+    formStateData.feedback = formDataObj.feedback;
+    inputEl.value = formStateData.email;
+    textareaEl.value = formStateData.feedback;
+  }
+}
