8e715bd33fd1b7b466a1315de84f3ce70acd7413
ArtemTeslenko <artem.teslenko.w126@gmail.com>
2022-08-13 19:06:10 +0300

    Update 02-video.js
    Fixed video rewind bug.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,5 @@
+// Артем, прошу звернути увагу на 13 рядок. Ще не звик читати документації, може щось не так зрозумів. У мене був баг, перемотка відео спрацьовувала не з першого разу, як я зрозумів з прочитаного - подія seeking реагує на перетягування користувачем повзунка перемотки, а подія seeked на клік миші у певному місці полоси завнтаження відео. Я зняв прослуховувач події play, при перемотці і проблема зникла, але я не зовсім зрозумів, яким чином вона заважала перемотувати відео.
+
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
@@ -8,6 +10,7 @@ const player = new Player(iframe);
 player.on('timeupdate', throttle(obtainCurrTime, 1000));
 player.on('play', onPlayClick);
 player.on('ended', toBeginning);
+player.on('seeked', removePlayListener);
 
 function obtainCurrTime() {
   player.getCurrentTime().then(function (time) {
@@ -22,4 +25,8 @@ function onPlayClick() {
 
 function toBeginning() {
   player.setCurrentTime(0).then(function () {});
+}
+
+function removePlayListener() {
+  player.off('play');
 }
