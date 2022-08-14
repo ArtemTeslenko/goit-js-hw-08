4e3fb63ec661d3f3880c29b53be657698d9954b1
ArtemTeslenko <artem.teslenko.w126@gmail.com>
2022-08-14 10:17:06 +0300

    Update 02-video.js
    Fixed playing by time from local storage, added checking of data accuracy, when getting from local storage.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,4 @@
-// Артем, прошу звернути увагу на 13 рядок. Ще не звик читати документації, може щось не так зрозумів. У мене був баг, перемотка відео спрацьовувала не з першого разу, як я зрозумів з прочитаного - подія seeking реагує на перетягування користувачем повзунка перемотки, а подія seeked на клік миші у певному місці полоси завнтаження відео. Я зняв прослуховувач події play, при перемотці і проблема зникла, але я не зовсім зрозумів, яким чином вона заважала перемотувати відео.
+//З приводу перевірки, я не став виводити повідомлення про помилку, мені здається, що юзеру воно мало що дасть, просто, якщо отримали не коректні дані, то відео починається з початку рядок 25-26.
 
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
@@ -7,10 +7,8 @@ const CURRENT_TIME = 'videoplayer-current-time';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+setTime();
 player.on('timeupdate', throttle(obtainCurrTime, 1000));
-player.on('play', onPlayClick);
-player.on('ended', toBeginning);
-player.on('seeked', removePlayListener);
 
 function obtainCurrTime() {
   player.getCurrentTime().then(function (time) {
@@ -18,15 +16,13 @@ function obtainCurrTime() {
   });
 }
 
-function onPlayClick() {
+function setTime() {
   const currentTime = Number(localStorage.getItem(CURRENT_TIME));
-  player.setCurrentTime(currentTime).then(function () {});
-}
 
-function toBeginning() {
-  player.setCurrentTime(0).then(function () {});
-}
-
-function removePlayListener() {
-  player.off('play');
+  try {
+    player.setCurrentTime(currentTime);
+  } catch (error) {
+    // window.alert(error.name, error.message);
+    player.setCurrentTime(currentTime);
+  }
 }
